81ca440f8d1a02d5e18805fe0d3118f602e370c8
frmnxhken <frmnxhken@gmail.com>
2025-05-07 13:58:01 +0700

    Add dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,17 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExcuseController;
 use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard.app');
-});
-
 Route::get('/login', [AdminController::class, 'login'])->name('login');
 Route::post('/login', [AdminController::class, 'authentication']);
 
 Route::middleware(['auth:admin'])->group(function() {
+    Route::get('/', [DashboardController::class, 'index']);
     Route::post('/logout', [AdminController::class, 'deauthentication']);
     Route::resource('/employee', UserController::class);
     Route::resource('/office', OfficeController::class);
