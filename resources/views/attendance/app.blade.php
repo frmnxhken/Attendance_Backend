ef91f4249dac8feadfd94f827e67ff2bc9be52de
Adyanullah <151485379+Adyanullah@users.noreply.github.com>
2025-05-10 15:19:45 +0700

    feat: add image to attendance page

--- resources/views/attendance/app.blade.php
+++ resources/views/attendance/app.blade.php
@@ -11,7 +11,7 @@
         </div>
     </div>
     <div class="row">
-        @foreach($attendances as $date => $records)
+        @foreach ($attendances as $date => $records)
             <div class="card mb-4">
                 <div class="card-body">
                     <div class="mb-3">
@@ -20,13 +20,78 @@
                         </h4>
                     </div>
                     <table class="table table-hover">
-                        @foreach($records as $attendance)
+                        @foreach ($records as $attendance)
                             <tr>
                                 <td>{{ $attendance->user->name }}</td>
-                                <td>Checkin: {{ $attendance->checkin ?? '--:--' }}</td>
-                                <td>Checkout: {{ $attendance->checkout ?? '--:--' }}</td>
                                 <td>
-                                    @if($attendance->status == 'present')
+                                    <div class="d-flex align-items-center gap-3">
+                                        <a href="#" data-bs-toggle="modal"
+                                            data-bs-target="#checkinModal{{ $attendance->id }}">
+                                            <img src="{{ is_null($attendance->checkin_photo) ? '' : asset($attendance->checkin_photo) }}"
+                                                alt="image" class="img-fluid avatar-md rounded" />
+                                        </a>
+                                        Checkin: {{ $attendance->checkin ?? '--:--' }}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="d-flex align-items-center gap-3">
+                                        <a href="#" data-bs-toggle="modal"
+                                            data-bs-target="#checkoutModal{{ $attendance->id }}">
+                                            <img src="{{ is_null($attendance->checkout_photo) ? '' : asset($attendance->checkout_photo) }}"
+                                                alt="image" class="img-fluid avatar-md rounded" />
+                                        </a>
+                                        Checkout: {{ $attendance->checkout ?? '--:--' }}
+                                    </div>
+                                </td>
+
+                                {{-- Check-in Modal --}}
+                                <div class="modal fade" id="checkinModal{{ $attendance->id }}" tabindex="-1"
+                                    aria-labelledby="checkinModalLabel{{ $attendance->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="checkinModalLabel{{ $attendance->id }}">
+                                                    Check-in: {{ $attendance->checkin ?? '--:--' }}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <img src="{{ is_null($attendance->checkin_photo) ? '' : asset($attendance->checkin_photo) }}"
+                                                    alt="Check-in photo" class="d-block w-100" />
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {{-- Check-out Modal --}}
+                                <div class="modal fade" id="checkoutModal{{ $attendance->id }}" tabindex="-1"
+                                    aria-labelledby="checkoutModalLabel{{ $attendance->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="checkoutModalLabel{{ $attendance->id }}">
+                                                    Check-out: {{ $attendance->checkout ?? '--:--' }}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <img src="{{ is_null($attendance->checkout_photo) ? '' : asset($attendance->checkout_photo) }}"
+                                                    alt="Check-out photo" class="d-block w-100" />
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <td>
+                                    @if ($attendance->status == 'present')
                                         <span class="badge bg-success">Present</span>
                                     @elseif($attendance->status == 'excuse')
                                         <span class="badge bg-warning">Excuse</span>
@@ -36,6 +101,7 @@
                                 </td>
                             </tr>
                         @endforeach
+
                     </table>
                 </div>
             </div>
