56691a797a996b054735a76b5621716981974e75
frmnxhken <frmnxhken@gmail.com>
2025-05-12 16:08:04 +0700

    Refac API route

--- routes/api.php
+++ routes/api.php
@@ -4,35 +4,33 @@ use App\Http\Controllers\API\AttendanceController;
 use App\Http\Controllers\API\ExcuseController;
 use App\Http\Controllers\API\HistoryController;
 use App\Http\Controllers\API\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/helloworld', function () {
-    return response()->json([
-        "message" => "Hello World!"
-    ]);
+Route::prefix('auth')->group(function () {
+    Route::post('/', [UserController::class, 'authentication']);
+    Route::post('/reset-password', [UserController::class, 'resetPassword']);
 });
 
-Route::post('/user/reset-password', [UserController::class, 'resetPassword']);
-Route::post('/auth', [UserController::class, "authentication"]);
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/profile', [UserController::class, 'getProfile']);
+    Route::prefix('user')->group(function () {
+        Route::get('/profile', [UserController::class, 'getProfile']);
+        Route::post('/logout', [UserController::class, 'deauthentication']);
+        Route::post('/update-photo', [UserController::class, 'updatePhoto']);
+        Route::post('/update-password', [UserController::class, 'updatePassword']);
+    });
 
-    Route::post('/logout', [UserController::class, 'deauthentication']);
+    Route::prefix('history')->group(function () {
+        Route::get('/recent', [HistoryController::class, 'recent']);
+        Route::get('/', [HistoryController::class, 'histories']);
+    });
 
-    Route::post('/user/update-photo', [UserController::class, 'updatePhoto']);
-    Route::post('/user/update-password', [UserController::class, 'updatePassword']);
+    Route::prefix('attendance')->group(function () {
+        Route::get('/status', [AttendanceController::class, 'checkStatus']);
+        Route::post('/checkin', [AttendanceController::class, 'checkIn']);
+        Route::post('/checkout', [AttendanceController::class, 'checkOut']);
+    });
 
-    Route::get('/history/recent', [HistoryController::class, 'recent']);
-    Route::get('/history', [HistoryController::class, 'histories']);
-
-    Route::get('/status', [AttendanceController::class, 'checkStatus']);
-    Route::post('/checkin', [AttendanceController::class, 'checkIn']);
-    Route::post('/checkout', [AttendanceController::class, 'checkOut']);
-
-    Route::post('/excuse/request', [ExcuseController::class, 'requestExcuse']);
+    Route::prefix('excuse')->group(function () {
+        Route::post('/request', [ExcuseController::class, 'requestExcuse']);
+    });
 });
