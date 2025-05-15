797fcd4daa84050344f92bcc1e7af8b255e52dca
frmnxhken <frmnxhken@gmail.com>
2025-05-15 10:32:20 +0700

    Add constrained on delete

--- database/migrations/2025_05_03_102441_create_attendances_table.php
+++ database/migrations/2025_05_03_102441_create_attendances_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->date('date');
             $table->time('checkin')->nullable();
             $table->double('checkin_long')->nullable();
--- database/migrations/2025_05_03_103214_create_work_balances_table.php
+++ database/migrations/2025_05_03_103214_create_work_balances_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('work_balances', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->integer('total_minutes');
             $table->timestamps();
         });
